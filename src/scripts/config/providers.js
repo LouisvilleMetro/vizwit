bc892b6639213194435931e9c1ba11b0fdfdfe81
mjhampton12182 <mary.hampton@louisvilleky.gov>
2017-04-25 13:37:09 -0400

    added dkan provider

--- src/scripts/config/providers.js
+++ src/scripts/config/providers.js
@@ -1,4 +1,5 @@
 module.exports = {
   socrata: require('../collections/socrata'),
-  carto: require('../collections/carto')
+  carto: require('../collections/carto'),
+  dkan: require('../collections/dkan')
 }
